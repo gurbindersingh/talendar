5910812bb6ded93b1b49cf86837bafa6ba51a793
Thomas Drescher <thomas.drescher@chello.at>
2019-04-15 15:21:28 +0200

    Add Initial Empty Backeend Template

---
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/BackendApplicationTests.java
@@ -0,0 +1,22 @@
+package at.ac.tuwien.sepm.groupphase.backend;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class BackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	    assertEquals("Hello Test", "Hello Test");
+	    assertThat("Hello Test", equalToIgnoringCase("HELLO TEST"));
+	}
+
+}
