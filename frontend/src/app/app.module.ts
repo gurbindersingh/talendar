130093c4b0d630d5e1edd4f3f1c3346e8c51b8a4
Gurbinder Singh <gurbinder.singh@student.tuwien.ac.at>
2019-05-02 16:26:17 +0200

    Add angular-calender dependency

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, NgbModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbModule,
+        BrowserAnimationsModule,
+        CalendarModule.forRoot({
+            provide: DateAdapter,
+            useFactory: adapterFactory,
+        }),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
