5b926eb339b2c3e6291344f3d0a4755916d4283b
Michael Klement <michael1.klement4@gmail.com>
2019-05-15 17:29:32 +0200

    #37438
    Validation completed

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/util/validator/Validator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/util/validator/Validator.java
@@ -9,5 +9,15 @@ public class Validator {
     public Validator(){}
 
     public void validateHoliday (Holiday holiday) throws InvalidEntityException {
+
+        if (holiday.getId() != null) {
+            throw new InvalidEntityException("id must be null");
+        }
+        if (holiday.getTrainerid() == null) {
+            throw new InvalidEntityException("Trainerid must not be null");
+        }
+        if (holiday.getHolidayStart().isAfter(holiday.getHolidayEnd())) {
+            throw new InvalidEntityException("Start of holiday must be before the end");
+        }
     }
 }
