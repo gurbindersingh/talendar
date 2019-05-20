e03031741b2569ab6f922d336b4c6202a76b5241
nikahol <nikita2kholodnyi@gmail.com>
2019-05-20 15:26:19 +0200

    [Issue #37046] Working on New Birthday Frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CalendarComponent } from './calendar/calendar.component';
+import { NewBirthdayComponent } from './new-birthday/new-birthday.component';
 
 const routes: Routes = [
     {
         path: '',
         component: CalendarComponent,
     },
+    {
+        path: 'addbirthday',
+        component: NewBirthdayComponent,
+
+    }
 ];
 
 @NgModule({
