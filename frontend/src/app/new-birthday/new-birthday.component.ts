229211960a1bb841b2665cd96602af11bbfe0f40
nikahol <nikita2kholodnyi@gmail.com>
2019-05-20 14:32:50 +0200

    beginning to setup front end us3

---
+++ frontend/src/app/new-birthday/new-birthday.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-birthday',
+  templateUrl: './new-birthday.component.html',
+  styleUrls: ['./new-birthday.component.scss']
+})
+export class NewBirthdayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
