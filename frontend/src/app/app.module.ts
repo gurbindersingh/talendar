c23de38b62c06a6b068964fd234096ef2e8ed3b0
nikahol <nikita2kholodnyi@gmail.com>
2019-05-21 20:55:00 +0200

    Functional Push

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -14,10 +13,11 @@ import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { AddCourseComponent } from './add-course/add-course.component';
+import { NewBirthdayComponent } from './new-birthday/new-birthday.component';
 import { AddPrivateMeetingComponent } from './add-private-meeting/add-private-meeting.component';
 
 @NgModule({
-    declarations: [AppComponent, CalendarComponent, AddCourseComponent, AddPrivateMeetingComponent],
+    declarations: [AppComponent, CalendarComponent, AddCourseComponent, AddPrivateMeetingComponent, NewBirthdayComponent],
     imports: [
         AppRoutingModule,
         BrowserAnimationsModule,
