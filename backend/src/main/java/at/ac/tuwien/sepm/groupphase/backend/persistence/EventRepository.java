7e497a5906fbb92c43ddf015398c1cb53f15c001
nikahol <nikita2kholodnyi@gmail.com>
2019-05-24 10:34:16 +0200

    US9: Small delete updates, method to find all courses

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/persistence/EventRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/persistence/EventRepository.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.sepm.groupphase.backend.persistence;
 
 import at.ac.tuwien.sepm.groupphase.backend.Entity.Event;
 import at.ac.tuwien.sepm.groupphase.backend.Entity.RoomUse;
+import at.ac.tuwien.sepm.groupphase.backend.enums.EventType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,6 +14,8 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<RoomUse> findByTrainer_IdAndRoomUses_BeginGreaterThanEqualAndDeletedIs(Long id, LocalDateTime now, boolean deleted);
+
+    List<Event> findByEventTypeEqualsAndDeletedIs(EventType eventType, boolean deleted);
 
     @Query(value = "update Event e set e.deleted = true where e.id = ?1", nativeQuery = true)
     void deleteThisEvent(Long id);
