1263c2a05e8e51e4c5d140ae964b00bcd75a28ef
Tobias Eidelpes <tobias@eidelpes.info>
2019-05-24 12:09:00 +0200

    Change coding style to sane defaults
    The automatic formatter in IntelliJ (Ctrl + Alt + L) now adds spaces
    after if-statements and does not put catch-statements on a new line.

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/EventService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/EventService.java
@@ -11,7 +11,6 @@ import at.ac.tuwien.sepm.groupphase.backend.persistence.HolidayRepository;
 import at.ac.tuwien.sepm.groupphase.backend.persistence.RoomUseRepository;
 import at.ac.tuwien.sepm.groupphase.backend.persistence.TrainerRepository;
 import at.ac.tuwien.sepm.groupphase.backend.service.IEventService;
-import at.ac.tuwien.sepm.groupphase.backend.service.exceptions.ServiceException;
 import at.ac.tuwien.sepm.groupphase.backend.service.exceptions.ValidationException;
 import at.ac.tuwien.sepm.groupphase.backend.util.validator.Validator;
 import at.ac.tuwien.sepm.groupphase.backend.util.validator.exceptions.InvalidEntityException;
@@ -21,7 +20,6 @@ import org.springframework.stereotype.Service;
 
 import org.slf4j.Logger;
 
-import javax.sql.rowset.serial.SerialException;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
@@ -36,7 +34,7 @@ public class EventService implements IEventService {
     private final HolidayRepository holidayRepository;
 
     @Autowired
-    public EventService(EventRepository eventRepository, Validator validator, RoomUseRepository roomUseRepository, TrainerRepository trainerRepository, HolidayRepository holidayRepository){
+    public EventService(EventRepository eventRepository, Validator validator, RoomUseRepository roomUseRepository, TrainerRepository trainerRepository, HolidayRepository holidayRepository) {
         this.eventRepository = eventRepository;
         this.validator = validator;
         this.roomUseRepository = roomUseRepository;
@@ -45,7 +43,7 @@ public class EventService implements IEventService {
     }
 
     @Override
-    public Event save (Event event) throws ValidationException, ServiceException {
+    public Event save(Event event) throws ValidationException {
         LOGGER.info("Prepare to save new Event");
         LocalDateTime now = LocalDateTime.now();
         event.setCreated(now);
@@ -53,38 +51,33 @@ public class EventService implements IEventService {
 
         try {
             Thread.sleep(1);
-        }
-        catch(InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        switch(event.getEventType()) {
+        switch (event.getEventType()) {
             case Birthday:
-                  try {
-                      validator.validateEvent(event);
-                      event.setTrainer(findTrainerForBirthday(event.getRoomUses(), event.getBirthdayType()));
-                      event = synchRoomUses(event);
-                  }
-                  catch(InvalidEntityException e) {
-                      throw new ValidationException("Given Birthday is invalid: " + e.getMessage(), e);
-                  }catch(TrainerNotAvailableException e){
-                      throw new ValidationException("There are no Trainers available for this birthday " + e.getMessage(),e);
-                  }
-
-                  break;
+                try {
+                    validator.validateEvent(event);
+                    event.setTrainer(findTrainerForBirthday(event.getRoomUses(), event.getBirthdayType()));
+                    event = synchRoomUses(event);
+                } catch (InvalidEntityException e) {
+                    throw new ValidationException("Given Birthday is invalid: " + e.getMessage(), e);
+                } catch (TrainerNotAvailableException e) {
+                    throw new ValidationException("There are no Trainers available for this birthday " + e.getMessage(), e);
+                }
+                break;
             case Course:
                 try {
                     validator.validateEvent(event);
-                }
-                catch(InvalidEntityException e) {
+                } catch (InvalidEntityException e) {
                     throw new ValidationException("Given Course is invalid: " + e.getMessage(), e);
                 }
                 break;
             case Rent:
                 try {
                     validator.validateEvent(event);
-                }
-                catch(InvalidEntityException e) {
+                } catch (InvalidEntityException e) {
                     throw new ValidationException("Given Rent is invalid: " + e.getMessage(), e);
                 }
                 break;
@@ -99,9 +92,9 @@ public class EventService implements IEventService {
                 }
                 break;
         }
-        try{
+        try {
             isAvailable(event.getRoomUses());
-        }catch(TimeNotAvailableException e){
+        } catch (TimeNotAvailableException e) {
             throw new ValidationException("The event is attempting to be booked during a different event: " + e.getMessage(), e);
         }
         return eventRepository.save(event);
@@ -110,8 +103,7 @@ public class EventService implements IEventService {
     public Trainer findTrainerForBirthday(List<RoomUse> roomUses, String birthdayType) throws TrainerNotAvailableException{
         List<Trainer> appropriateTrainers = trainerRepository.findByBirthdayTypes(birthdayType);
         Collections.shuffle(appropriateTrainers);
-        for(Trainer t: appropriateTrainers
-            ) {
+        for (Trainer t : appropriateTrainers) {
             try {
                 trainerAvailable(t, roomUses);
                 return t;
@@ -121,9 +113,9 @@ public class EventService implements IEventService {
         }
         return null;
     }
-    public Event synchRoomUses(Event event){
-        for(RoomUse x: event.getRoomUses()
-            ) {
+
+    public Event synchRoomUses(Event event) {
+        for (RoomUse x : event.getRoomUses()) {
             x.setEvent(event);
         }
         return event;
@@ -133,17 +125,24 @@ public class EventService implements IEventService {
         List<RoomUse> trainersEvents = eventRepository.findByTrainer_IdAndRoomUses_BeginGreaterThanEqual(trainer.getId(), LocalDateTime.now());
         List<Holiday> trainerHoliday = holidayRepository.findByTrainer_Id(trainer.getId());
 
-        for(RoomUse x: roomUses
-            ) {
-            for(RoomUse db: trainersEvents
-                ) {
-                if(x.getBegin().isAfter(db.getBegin()) && x.getBegin().isBefore(db.getEnd()) || x.getEnd().isBefore(db.getEnd()) && x.getEnd().isAfter(db.getBegin()) || x.getBegin().isBefore(db.getBegin()) && x.getEnd().isAfter(db.getEnd())) {
+        for (RoomUse x : roomUses) {
+            for (RoomUse db : trainersEvents) {
+                if (x.getBegin().isAfter(db.getBegin()) &&
+                        x.getBegin().isBefore(db.getEnd()) ||
+                        x.getEnd().isBefore(db.getEnd()) &&
+                        x.getEnd().isAfter(db.getBegin()) ||
+                        x.getBegin().isBefore(db.getBegin()) &&
+                        x.getEnd().isAfter(db.getEnd())) {
                     throw new TrainerNotAvailableException("The specified trainer is not available for the allocated time frame");
                 }
             }
-            for(Holiday db: trainerHoliday
-                ) {
-                if(x.getBegin().isAfter(db.getHolidayStart()) && x.getBegin().isBefore(db.getHolidayEnd()) || x.getEnd().isBefore(db.getHolidayEnd()) && x.getEnd().isAfter(db.getHolidayStart()) || x.getBegin().isBefore(db.getHolidayStart()) && x.getEnd().isAfter(db.getHolidayEnd())) {
+            for (Holiday db : trainerHoliday) {
+                if (x.getBegin().isAfter(db.getHolidayStart()) &&
+                        x.getBegin().isBefore(db.getHolidayEnd()) ||
+                        x.getEnd().isBefore(db.getHolidayEnd()) &&
+                        x.getEnd().isAfter(db.getHolidayStart()) ||
+                        x.getBegin().isBefore(db.getHolidayStart()) &&
+                        x.getEnd().isAfter(db.getHolidayEnd())) {
                     throw new TrainerNotAvailableException("The specified trainer is not available for the allocated time frame");
                 }
             }
@@ -153,13 +152,17 @@ public class EventService implements IEventService {
     public void isAvailable(List<RoomUse> roomUseList) throws TimeNotAvailableException {
         LocalDateTime now = LocalDateTime.now();
         List<RoomUse> dbRooms = roomUseRepository.findByBeginGreaterThanEqual(now);
-        for(RoomUse x: roomUseList
-            ) {
-            for(RoomUse db: roomUseList
-                ) {
-                if(x.getRoom() == db.getRoom()) {
-                    if(x.getBegin().isAfter(db.getBegin()) && x.getBegin().isBefore(db.getEnd()) || x.getEnd().isBefore(db.getEnd()) && x.getEnd().isAfter(db.getBegin()) || x.getBegin().isBefore(db.getBegin()) && x.getEnd().isAfter(db.getEnd())) {
-                        throw new TimeNotAvailableException("The Timeslot attempting to be booked is invalid. Attempted Booking: " + x.toString());
+        for (RoomUse x : roomUseList) {
+            for (RoomUse db : roomUseList) {
+                if (x.getRoom() == db.getRoom()) {
+                    if (x.getBegin().isAfter(db.getBegin()) &&
+                            x.getBegin().isBefore(db.getEnd()) ||
+                            x.getEnd().isBefore(db.getEnd()) &&
+                            x.getEnd().isAfter(db.getBegin()) ||
+                            x.getBegin().isBefore(db.getBegin()) &&
+                            x.getEnd().isAfter(db.getEnd())) {
+                        throw new TimeNotAvailableException("The Timeslot attempting to be booked is invalid. Attempted Booking: " +
+                                x.toString());
                     }
                 }
             }
