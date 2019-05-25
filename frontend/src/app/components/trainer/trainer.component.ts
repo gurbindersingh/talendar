8a670a7cd3d23deccabd300380f0ade6cab698a4
ThomasDre <drescher.thomas@outlook.com>
2019-05-25 11:58:38 +0200

    Make Add Page Context Aware (Save or Edit Mode) [#38292]

--- frontend/src/app/components/trainer/trainer.component.ts
+++ frontend/src/app/components/trainer/trainer.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { TrainerClient } from 'src/app/rest/trainer-client';
 import { Trainer } from 'src/app/models/trainer';
-import { HttpResponse } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
     selector: 'app-trainer',
@@ -10,27 +11,91 @@ import { HttpResponse } from '@angular/common/http';
     styleUrls: ['./trainer.component.scss'],
 })
 export class TrainerComponent implements OnInit {
-    private title = 'Betreuer erstellen';
+    private title: string;
     private trainer: Trainer = new Trainer();
+    private isSaveMode: boolean;
+    private btnContextDescription: string;
     private errorMsg: string;
     private successMsg: string;
 
-    constructor(private trainerClient: TrainerClient) {}
+    constructor(
+        private trainerClient: TrainerClient,
+        private route: ActivatedRoute,
+        private router: Router,
+        private location: Location
+    ) {}
 
-    ngOnInit() {}
+    /**
+     * Upon loading this component, we will either save a new trainer (no id query param)
+     * -> .../trainer
+     * or we will edit an existing trainer (then there is an id query param)
+     * -> .../trainer?id=xyz
+     */
+    ngOnInit() {
+        console.log('Initialize trainer component');
+
+        const id: bigint = this.route.snapshot.queryParams.id;
+
+        if (id === undefined) {
+            this.title = 'Trainer Erstellen';
+            this.btnContextDescription = 'Neuen Trainer erstellen';
+            this.isSaveMode = true;
+        } else {
+            this.title = 'Trainer Bearbeiten';
+            this.btnContextDescription = 'Änderungen speichern';
+            this.isSaveMode = false;
+            this.trainerClient.getById(id).subscribe(
+                (data: Trainer) => {
+                    console.log(data);
+                    this.trainer = data;
+                },
+                (error: Error) => {
+                    /**
+                     * Even though this error situation should be very rare,
+                     * this is not the smoothest solution.
+                     * But what to do?!?
+                     * just stay here and continue with save made (imo not sensible too)
+                     */
+                    alert(
+                        'The requested trainer could not be laoded: ' +
+                            error.message
+                    );
+                    this.location.back();
+                }
+            );
+        }
+    }
 
     public postTrainer(form: NgForm): void {
         console.log('Pass Form Data To Rest Client');
-        this.trainerClient.postNewTrainer(this.trainer).subscribe(
-            (data: Trainer) => {
-                console.log(data);
-                this.successMsg = 'Der Betreuer wurde erfolgreich gespeichert';
-            },
-            (error) => {
-                console.log(error);
-                this.errorMsg = 'Der Betreuer konnte nicht angelegt werden!';
-            }
-        );
+
+        if (this.isSaveMode) {
+            this.trainerClient.postNewTrainer(this.trainer).subscribe(
+                (data: Trainer) => {
+                    console.log(data);
+                    this.successMsg =
+                        'Der Betreuer wurde erfolgreich gespeichert';
+                },
+                (error: Error) => {
+                    console.log(error.message);
+                    this.errorMsg =
+                        'Der Betreuer konnte nicht angelegt werden!';
+                }
+            );
+        } else {
+            this.trainerClient.update(this.trainer).subscribe(
+                (data: Trainer) => {
+                    console.log(data);
+                    this.successMsg =
+                        'Der Betreuer wurde erfolgreich aktualisiert';
+                },
+                (error: Error) => {
+                    console.log(error.message);
+                    this.errorMsg =
+                        'Der Betreuer konnte nicht erfolgreich aktualisiert werden';
+                }
+            );
+        }
     }
 
     public isCompleted(): boolean {
@@ -61,5 +126,9 @@ export class TrainerComponent implements OnInit {
     public clearInfoMsg(): void {
         this.errorMsg = undefined;
         this.successMsg = undefined;
+    }
+
+    public cancel(): void {
+        this.location.back();
     }
 }
