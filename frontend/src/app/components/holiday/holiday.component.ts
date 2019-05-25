78d31773901029d25027e086ee5d767ea533d11c
Michael Klement <michael1.klement4@gmail.com>
2019-05-25 10:52:01 +0200

    Mostly finished task #37539
    Task basically done, just needs more testing to make sure everything works as intendet

--- frontend/src/app/components/holiday/holiday.component.ts
+++ frontend/src/app/components/holiday/holiday.component.ts
@@ -21,7 +21,14 @@ export class HolidayComponent implements OnInit {
     private holiday: Holiday = new Holiday();
     private trainer: Trainer = new Trainer();
 
-    constructor(private holidayClient: HolidayClient) {
+    startDate: NgbDateStruct;
+    startTime: NgbTimeStruct;
+    endDate: NgbDateStruct;
+    endTime: NgbTimeStruct;
+
+    constructor(
+        private holidayClient: HolidayClient,
+        private parserFormatter: NgbDateParserFormatter) {
     }
 
     ngOnInit() {
@@ -31,6 +38,10 @@ export class HolidayComponent implements OnInit {
         console.log('Pass Form Data To Rest Client');
         this.holiday.id = null;
         this.holiday.trainer = this.trainer;
+
+        this.holiday.holidayStart = this.dateToString(this.startDate, this.startTime);
+        this.holiday.holidayEnd = this.dateToString(this.endDate, this.endTime);
+
         this.holidayClient.postNewHoliday(this.holiday).subscribe(
             (data: Holiday) => {
                 console.log(data);
@@ -42,8 +53,119 @@ export class HolidayComponent implements OnInit {
             }
         );
     }
+
     public clearInfoMsg(): void {
       this.errorMsg = undefined;
       this.successMsg = undefined;
+    }
+
+    private dateToString(date: NgbDateStruct, time: NgbTimeStruct) {
+        let stringMinute = '';
+        let stringHour = '';
+        let isChangedHour = false;
+        let isChangedMinute = false;
+
+        if (time.minute === 0) {
+            stringMinute = '00';
+            isChangedMinute = true;
+        }
+        if (time.hour === 0) {
+            stringHour = '00';
+            isChangedHour = true;
+        }
+        if (time.hour === 1) {
+            stringHour = '01';
+            isChangedHour = true;
+        }
+        if (time.hour === 2) {
+            stringHour = '02';
+            isChangedHour = true;
+        }
+        if (time.hour === 3) {
+            stringHour = '03';
+            isChangedHour = true;
+        }
+        if (time.hour === 4) {
+            stringHour = '04';
+            isChangedHour = true;
+        }
+        if (time.hour === 5) {
+            stringHour = '05';
+            isChangedHour = true;
+        }
+        if (time.hour === 6) {
+            stringHour = '06';
+            isChangedHour = true;
+        }
+        if (time.hour === 7) {
+            stringHour = '07';
+            isChangedHour = true;
+        }
+        if (time.hour === 8) {
+            stringHour = '08';
+            isChangedHour = true;
+        }
+        if (time.hour === 9) {
+            stringHour = '09';
+            isChangedHour = true;
+        }
+
+        if (isChangedHour && isChangedMinute) {
+            return (
+                this.parserFormatter.format(date) +
+                'T' +
+                stringHour +
+                ':' +
+                stringMinute +
+                ':00'
+            );
+        }
+
+        if (isChangedHour) {
+            return (
+                this.parserFormatter.format(date) +
+                'T' +
+                stringHour +
+                ':' +
+                time.minute +
+                ':00'
+            );
+        }
+
+        if (isChangedMinute) {
+            return (
+                this.parserFormatter.format(date) +
+                'T' +
+                time.hour +
+                ':' +
+                time.minute +
+                ':00'
+            );
+        }
+
+        return (
+            this.parserFormatter.format(date) +
+            'T' +
+            time.hour +
+            ':' +
+            time.minute +
+            ':00'
+        );
+    }
+
+    public isCompleted(): boolean {
+        if (this.startDate === undefined) {
+            return false;
+        }
+        if (this.startTime === undefined) {
+            return false;
+        }
+        if (this.endDate === undefined) {
+            return false;
+        }
+        if (this.endTime === undefined) {
+            return false;
+        }
+        return true;
     }
 }
