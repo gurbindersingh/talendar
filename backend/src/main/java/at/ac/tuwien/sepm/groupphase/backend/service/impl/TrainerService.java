be2be961d8bc04004d57eeec2a7c36f218acf4a2
ThomasDre <drescher.thomas@outlook.com>
2019-05-25 11:53:31 +0200

    Fix Bug For Missing Transactional Context
    Without transaction spring is not able to save changes to a managed entity

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/TrainerService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/TrainerService.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.PersistenceException;
 import java.time.LocalDateTime;
@@ -68,6 +69,7 @@ public class TrainerService implements ITrainerService {
     }
 
 
+    @Transactional
     @Override
     public Trainer update (Trainer trainer) throws ServiceException, ValidationException, NotFoundException {
         LOGGER.info("Prepare update for trainer with id {}.\nProposed updated entity: {}", trainer.getId(), trainer);
