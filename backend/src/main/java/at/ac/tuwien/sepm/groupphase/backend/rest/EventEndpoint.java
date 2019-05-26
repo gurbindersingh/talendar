4cb04a6deef0adfaf61a360ef7a624bbe1da499e
Marc-Aurel Pfaffenlehner <marc1995.aurel@gmail.com>
2019-05-26 15:44:31 +0200

    [#] Bugfix
    Why: missing return statement

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/rest/EventEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/rest/EventEndpoint.java
@@ -40,14 +40,13 @@ public class EventEndpoint {
     public EventDto createNewEvent (@RequestBody EventDto eventDto) throws BackendException {
         LOGGER.info("Incoming POST Request for an Event with type: " + eventDto.toString());
         try {
-            return eventMapper.entityToEventDto(eventService.save(eventMapper.dtoToEventEntity(
-                eventDto)));
-        }
-        catch(ValidationException e) {
+            return eventMapper.entityToEventDto(eventService.save(eventMapper.dtoToEventEntity(eventDto)));
+        }catch(ValidationException e) {
             LOGGER.error(e.getMessage(), e);
             throw new BackendException(e.getMessage(), e);
         }catch(ServiceException e){
             LOGGER.error(e.getMessage(), e);
+            throw new BackendException(e.getMessage(), e);
         }
     }
 
