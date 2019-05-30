9c34126479bf1105cd6e66298c3c0fe89a9189ac
Marc-Aurel Pfaffenlehner <marc1995.aurel@gmail.com>
2019-05-30 17:07:07 +0200

    [#39118] frontend to backend for course sign

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/rest/EventEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/rest/EventEndpoint.java
@@ -73,6 +73,25 @@ public class EventEndpoint {
         }
     }
 
+    @RequestMapping(value = "/customers", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public EventDto updateEventCustomers(@RequestBody EventDto eventDto) throws BackendException {
+        LOGGER.info("Incoming PUT Request (update customers) for an Event with type: " + eventDto.toString());
+        try {
+            return eventMapper.entityToEventDto(eventService.updateCustomers(eventMapper.dtoToEventEntity(eventDto)));
+        } catch(ValidationException e){
+            LOGGER.error(e.getMessage(), e);
+            throw new BackendException(e.getMessage(), e);
+        } catch(ServiceException e){
+            LOGGER.error(e.getMessage(), e);
+            throw new BackendException(e.getMessage(), e);
+        } catch(NotFoundException e){
+            LOGGER.error(e.getMessage(), e);
+            throw new BackendException(e.getMessage(), e);
+        }
+    }
+
+
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void cancelEvent(@PathVariable("id") Long id) throws BackendException{
         LOGGER.info("Incoming DELETE Request for an Event with id " + id);
