7385f65342ca926f08db9ac46d28d206581d0700
ThomasDre <drescher.thomas@outlook.com>
2019-06-06 01:18:33 +0200

    Add Missing Pieces For Login In Frontend (#39554)

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+    constructor(private authenticationService: AuthenticationService) {}
 
-  constructor() { }
+    email: string;
+    password: string;
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
+    login(): void {
+        this.authenticationService.login(this.email, this.password).subscribe(
+            (data) => {
+                console.log('Login After Request Success: ' + data);
+            },
+            (error: Error) => {
+                console.log('Login After Request Failure: ' + error.message);
+            }
+        );
+    }
 }
