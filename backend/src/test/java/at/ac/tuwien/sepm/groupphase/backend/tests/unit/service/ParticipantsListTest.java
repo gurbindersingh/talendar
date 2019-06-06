99d5111c2a81b2eb18e6b830220bff8928555ffa
Tobias Eidelpes <tobias@eidelpes.info>
2019-06-06 19:08:51 +0200

    Add Testclass to allow easy viewing of generated pdf file

---
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/tests/unit/service/ParticipantsListTest.java
@@ -0,0 +1,33 @@
+package at.ac.tuwien.sepm.groupphase.backend.tests.unit.service;
+
+import at.ac.tuwien.sepm.groupphase.backend.Entity.Customer;
+import at.ac.tuwien.sepm.groupphase.backend.rest.dto.CustomerDto;
+import at.ac.tuwien.sepm.groupphase.backend.schedule.ParticipantsList;
+import at.ac.tuwien.sepm.groupphase.backend.testDataCreation.FakeData;
+import at.ac.tuwien.sepm.groupphase.backend.util.mapper.CustomerMapper;
+import at.ac.tuwien.sepm.groupphase.backend.util.mapper.CustomerMapperImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class ParticipantsListTest {
+
+    @Autowired
+    ParticipantsList participantsList;
+
+    @Autowired
+    private static FakeData faker = new FakeData();
+
+    @Test
+    public void createCustomer_thenCreateBill() {
+        Customer newCustomer = faker.fakeNewCustomerEntity();
+        System.out.println(newCustomer.toString());
+        CustomerMapper mapper = new CustomerMapperImpl();
+        CustomerDto newCustomerDto = mapper.entityToCustomerDto(newCustomer);
+        participantsList.createBill(newCustomerDto);
+    }
+}
