b4b94e1b8ea92717074dcaadad7cf0a671d4677e
ThomasDre <drescher.thomas@outlook.com>
2019-06-08 19:21:28 +0200

    Get User Logged Out After Token Expiration

--- frontend/src/app/http-interceptors/simple-http-interceptor.ts
+++ frontend/src/app/http-interceptors/simple-http-interceptor.ts
@@ -9,10 +9,17 @@ import { Observable, EMPTY, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { SessionStorageService } from '../services/session-storage-service';
 import { Injectable } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
+import { NotificationService } from '../services/notification.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class SimpleHttpInterceptor implements HttpInterceptor {
-    constructor(private sessionService: SessionStorageService) {}
+    constructor(
+        private sessionService: SessionStorageService,
+        private authenticationService: AuthenticationService,
+        private router: Router
+    ) {}
 
     intercept(
         req: HttpRequest<any>,
@@ -26,10 +33,18 @@ export class SimpleHttpInterceptor implements HttpInterceptor {
 
             console.log('Interceptor: Token = ' + token);
 
+            // check if token is expired, if yes, logout (which will notigy logout action)
+            if (token != null) {
+                const parsedToken = JSON.parse(atob(token.split('.')[1]));
+                if (parsedToken.exp < Date.now() / 1000) {
+                    this.authenticationService.logout();
+                }
+            }
+
             return next.handle(authReq).pipe(
                 catchError((error: HttpErrorResponse, caught) => {
                     if (error.status === 401) {
-                        location.pathname = '/login';
+                        this.router.navigate(['/login']);
                     }
                     throw error;
                 })
