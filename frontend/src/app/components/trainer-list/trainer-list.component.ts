22e5f5f9d74ecdecbdc15d0f50284ab513c3278d
Gurbinder Singh <gurbinder.singh@student.tuwien.ac.at>
2019-06-15 19:49:47 +0200

    [#40341] Add Pagination to Trainer List
    Why:
    * For better UX as the list can (technically) get very long

--- frontend/src/app/components/trainer-list/trainer-list.component.ts
+++ frontend/src/app/components/trainer-list/trainer-list.component.ts
@@ -65,45 +65,53 @@ export class NgbdModalConfirm {
     styleUrls: ['./trainer-list.component.scss'],
 })
 export class TrainerListComponent implements OnInit {
+    title = 'Trainerliste';
+    trainerList: Trainer[] = [];
+    filteredTrainerList: Trainer[] = [];
+    trainerListPage: Trainer[] = [];
+    currentPage = 1;
+    itemsPerPage = 10;
+
     constructor(
         private trainerClient: TrainerClient,
-        private _modalService: NgbModal
+        private modalService: NgbModal
     ) {}
-
-    private title = 'Trainerliste';
-    private trainerList: Trainer[] = [];
-
-    open(trainer: Trainer) {
-        const modalRef = this._modalService.open(NgbdModalConfirm);
-        modalRef.componentInstance.trainer = trainer;
-        modalRef.result.then(
-            () => {
-                // on close
-                console.log('Trying to DELETE Trainer with id ' + trainer.id);
-                this.trainerClient.deleteTrainer(trainer.id).subscribe(
-                    () => {
-                        this.ngOnInit();
-                    },
-                    (error) => {
-                        console.log(error);
-                    }
-                  );
-            },
-            () => {
-                // on dismiss
-            }
-        );
-    }
 
     ngOnInit() {
         console.log('Init Trainer List');
         this.trainerClient.getAll().subscribe(
             (list: Trainer[]) => {
                 this.trainerList = list;
+                this.filteredTrainerList = this.trainerList;
+                this.updateListPage();
             },
             (error) => {
-                console.log(error);
+                console.error(error);
             }
         );
+    }
+
+    updateListPage(page?: number) {
+        this.trainerListPage = this.filteredTrainerList.slice(
+            (this.currentPage - 1) * this.itemsPerPage,
+            this.currentPage * this.itemsPerPage
+        );
+    }
+
+    open(trainer: Trainer) {
+        const modalRef = this.modalService.open(NgbdModalConfirm);
+        modalRef.componentInstance.trainer = trainer;
+        modalRef.result.then(() => {
+            // on close
+            console.log('Trying to DELETE Trainer with id ' + trainer.id);
+            this.trainerClient.deleteTrainer(trainer.id).subscribe(
+                () => {
+                    this.ngOnInit();
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+        });
     }
 }
