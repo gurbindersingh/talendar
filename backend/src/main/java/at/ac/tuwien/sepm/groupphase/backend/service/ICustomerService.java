af4f7f5dd1b43efccd15f1b7ac498c92b4456b3f
Marc-Aurel Pfaffenlehner <marc1995.aurel@gmail.com>
2019-06-24 12:17:43 +0200

    added missed javadoc in service interfaces

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/ICustomerService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/ICustomerService.java
@@ -6,5 +6,9 @@ import org.springframework.stereotype.Component;
 @Component
 public interface ICustomerService {
 
+    /**
+     * Set wantsEmail to false
+     * @param email to find whose variable (wants emeail) has to be set to false
+     */
     void setWantsEmailFalse(String email);
 }
