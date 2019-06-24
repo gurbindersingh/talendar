e03aac98ede3347beae291ba0e963dd966bce507
ThomasDre <drescher.thomas@outlook.com>
2019-06-24 11:46:08 +0200

    Fix Bug That Same Tag Could Be Created Twice

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/persistence/TagRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/persistence/TagRepository.java
@@ -4,7 +4,10 @@ import at.ac.tuwien.sepm.groupphase.backend.Entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag,Long> {
 
+    Optional<Tag> findByTag(String tag);
 }
