0303f751f3616b36caf3be87db2a12f722aa4af3
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2019-08-14 19:19:03 +0200

    Add reCaptcha to Consultation Component

--- frontend/src/app/components/recaptcha/recaptcha.component.ts
+++ frontend/src/app/components/recaptcha/recaptcha.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'app-recaptcha',
     templateUrl: './recaptcha.component.html',
     styleUrls: ['./recaptcha.component.scss'],
 })
-export class RecaptchaComponent implements OnInit {
+export class RecaptchaComponent implements OnInit, OnDestroy {
+    form: HTMLFormElement;
+    script: HTMLScriptElement;
+
     constructor() {}
 
     ngOnInit() {}
+
+    ngOnDestroy(): void {}
 }
